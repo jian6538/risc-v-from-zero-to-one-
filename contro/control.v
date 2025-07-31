3ecd07ace596536786bb40027ec79071015d976d
jian6538 <56261153+jian6538@users.noreply.github.com>
2025-07-31 15:29:15 +0800

    Update control.v

--- contro/control.v
+++ contro/control.v
@@ -19,6 +19,12 @@ module ControlUnit (
     localparam OPCODE_LW    = 7'b0000011;
     localparam OPCODE_SW    = 7'b0100011;
     localparam OPCODE_BEQ   = 7'b1100011;
+    localparam OPCODE_ADDI  = 7'b0010011;
+    localparam OPCODE_JAL   = 7'b1101111;
+    localparam OPCODE_JALR  = 7'b1100111;
+    localparam OPCODE_LUI   = 7'b0110111;
+    localparam OPCODE_AUIPC = 7'b0010111;
+
 
     // Combinational logic to generate control signals based on opcode
     always @(*) begin
@@ -32,6 +38,53 @@ module ControlUnit (
                 Branch   = 0;
                 ALUOp    = 2'b10; // ALU Decoder will look at funct fields
             end
+            OPCODE_ADDI: begin
+                RegWrite = 1;
+                ALUSrc   = 1;  // Use immediate
+                MemRead  = 0;
+                MemWrite = 0;
+                MemToReg = 0;  // Result from ALU
+                Branch   = 0;
+                ALUOp    = 2'b00; // ALU will do ADD
+            end
+            OPCODE_JAL: begin
+                RegWrite = 1;      // Write PC+4 to rd
+                ALUSrc   = 0;      // Don't care
+                MemRead  = 0;
+                MemWrite = 0;
+                MemToReg = 0;      // ALU can compute PC+imm
+                Branch   = 0;      // PC change handled externally
+                ALUOp    = 2'b00;  // ADD for PC + imm
+            end
+            OPCODE_JALR: begin
+                RegWrite = 1;
+                ALUSrc   = 1;      // Use immediate for offset
+                MemRead  = 0;
+                MemWrite = 0;
+                MemToReg = 0;
+                Branch   = 0;
+                ALUOp    = 2'b00;
+            end
+            OPCODE_LUI: begin
+                RegWrite = 1;
+                ALUSrc   = 1;
+                MemRead  = 0;
+                MemWrite = 0;
+                MemToReg = 0;      // Result is immediate, may require bypass
+                Branch   = 0;
+                ALUOp    = 2'b11;  // Special ALU operation (may be passthrough)
+            end
+            OPCODE_AUIPC: begin
+                RegWrite = 1;
+                ALUSrc   = 1;
+                MemRead  = 0;
+                MemWrite = 0;
+                MemToReg = 0;
+                Branch   = 0;
+                ALUOp    = 2'b00;  // ALU adds PC + imm
+            end
+
+
             OPCODE_LW: begin
                 RegWrite = 1;
                 ALUSrc   = 1; // Operand B from immediate value
